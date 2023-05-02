e87842651de2b00707788cf9ca4159256298cc38
Kevin Arifin <kevarifin14@gmail.com>
2023-05-01 20:39:38 -0700

    add constants for api urls

---
+++ src/api/constants.ts
@@ -0,0 +1,3 @@
+export const LOCALNET_API_URL = "http://localhost:3001";
+export const DEVNET_API_URL = "https://dev.underdogprotocol.com";
+export const MAINNET_API_URL = "https://api.underdogprotocol.com";
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,4 @@ export * from "./pagination";
 export * from "./sort";
 export * from "./transaction";
 export * from "./params";
+export * from "./constants";
