36343e7905eb53803e17a0708fb6f3c2bad09b22
Kevin Arifin <kevarifin14@gmail.com>
2023-05-01 19:06:48 -0700

    fix types declaration not being included in package

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"], // Entry point of your library
-  format: ["cjs", "esm"], // Output formats
-  clean: true, // Clean the output directory before building
-  dts: true, // Generate TypeScript declaration files
+  entry: ["src/index.ts"], 
+  format: ["cjs", "esm"],
+  clean: true, 
+  dts: true, 
+  sourcemap: true, 
 });
