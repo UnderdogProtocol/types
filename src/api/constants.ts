14d097f6a7d782b267d240e168dfc1ebcc535fee
Kevin Arifin <kevarifin14@gmail.com>
2023-05-01 21:01:51 -0700

    add mapping of network to api url

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,11 @@
+import { NetworkEnum } from "../models";
+
 export const LOCALNET_API_URL = "http://localhost:3001";
 export const DEVNET_API_URL = "https://dev.underdogprotocol.com";
 export const MAINNET_API_URL = "https://api.underdogprotocol.com";
+
+export const networkToUnderdogApiEndpoints = {
+  [NetworkEnum.Localnet]: LOCALNET_API_URL,
+  [NetworkEnum.Devnet]: DEVNET_API_URL,
+  [NetworkEnum.Mainnet]: MAINNET_API_URL,
+}
