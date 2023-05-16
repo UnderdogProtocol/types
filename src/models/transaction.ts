faad2ec65326aa890c2e6539832b9d1d6f6f558f
Kevin Arifin <kevarifin14@gmail.com>
2023-05-16 13:32:21 -0700

    add createdAt to transactionSchema

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -7,6 +7,7 @@ export const transactionSchema = z.object({
   id: z.string(),
   status: z.string(),
   type: z.string(),
+  createdAt: z.string(),
 });
 
 export type Transaction = z.infer<typeof transactionSchema>;
