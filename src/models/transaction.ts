e051e5bd5010b7ea322080c25e67f865b1e78562
Kevin Arifin <kevarifin14@gmail.com>
2023-05-21 11:00:50 -0700

    add walletAddress to transaction model

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -1,12 +1,15 @@
 import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
 import { z } from "zod";
 
+import { publicKeyValueSchema } from "./metadata";
+
 extendZodWithOpenApi(z);
 
 export const transactionSchema = z.object({
   id: z.string(),
   status: z.string(),
   type: z.string(),
+  walletAddress: publicKeyValueSchema,
   data: z.string().optional(),
   createdAt: z.string(),
 });
