ea75709806f0afae746bd744d7d276f509e54c12
Kevin Arifin <kevarifin14@gmail.com>
2023-06-14 13:54:41 -0700

    models and validations for keys fixes

--- src/models/key.ts
+++ src/models/key.ts
@@ -5,7 +5,7 @@ import { networkEnumSchema, publicKeyValueSchema } from "./metadata";
 export const keySchema = z.object({
   prefix: z.string(),
   walletAddress: publicKeyValueSchema,
-  network: networkEnumSchema,
+  network: networkEnumSchema.optional(),
 });
 
 export type Key = z.infer<typeof keySchema>;
--- src/validations/key.ts
+++ src/validations/key.ts
@@ -1,10 +1,16 @@
 import { z } from "zod";
 
 import { keyPaginatedResponseSchema, paginatedQuerySchema } from "../api";
-import { keySchema, networkEnumSchema } from "../models";
+import { idSchema, keySchema, networkEnumSchema } from "../models";
+
+export const createKeyResponseSchema = z.string();
+export type CreateKeyResponse = z.infer<typeof createKeyResponseSchema>;
 
 export const getKeysRequestSchema = z.object({
   query: paginatedQuerySchema,
+  params: z.object({
+    orgId: idSchema,
+  }),
 });
 export type GetKeysRequest = z.infer<typeof getKeysRequestSchema>;
 
@@ -14,6 +20,7 @@ export type GetKeysResponse = z.infer<typeof getKeysResponseSchema>;
 export const updateKeyRequestSchema = z.object({
   params: z.object({
     prefix: z.string(),
+    orgId: idSchema,
   }),
   body: z.object({
     network: networkEnumSchema,
@@ -27,6 +34,7 @@ export type UpdateKeyResponse = z.infer<typeof updateKeyResponseSchema>;
 export const deleteKeyRequestSchema = z.object({
   params: z.object({
     prefix: z.string(),
+    orgId: idSchema,
   }),
 });
 export type DeleteKeyRequest = z.infer<typeof deleteKeyRequestSchema>;
