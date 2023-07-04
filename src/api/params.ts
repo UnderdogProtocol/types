b7299ea8e2aa1bfef8c7721a660b4d1b63626877
Kevin Arifin <kevarifin14@gmail.com>
2023-07-03 18:26:13 -0700

    remove required from project params

--- src/api/params.ts
+++ src/api/params.ts
@@ -5,26 +5,24 @@ import { idSchema } from "../models";
 
 extendZodWithOpenApi(z);
 
-export const projectParamsSchema = z
-  .object({
-    type: z
-      .string()
-      .optional()
-      .refine((val) => val === "t" || val === "n" || val === "c" || val === undefined, {
-        message: "Value must be 't' for transferable, 'n' for non-transferable, or 'c' for compressed",
-      })
-      .transform((val) =>
-        val === undefined
-          ? { transferable: true, compressed: true }
-          : { transferable: val === "t", compressed: val === "c" }
-      )
-      .openapi({
-        type: "string",
-        description: "Value must be 't' for transferable, 'n' for non-transferable, or 'c' for compressed",
-      }),
-    projectId: idSchema,
-  })
-  .required();
+export const projectParamsSchema = z.object({
+  type: z
+    .string()
+    .optional()
+    .refine((val) => val === "t" || val === "n" || val === "c" || val === undefined, {
+      message: "Value must be 't' for transferable, 'n' for non-transferable, or 'c' for compressed",
+    })
+    .transform((val) =>
+      val === undefined
+        ? { transferable: true, compressed: true }
+        : { transferable: val === "t", compressed: val === "c" }
+    )
+    .openapi({
+      type: "string",
+      description: "Value must be 't' for transferable, 'n' for non-transferable, or 'c' for compressed",
+    }),
+  projectId: idSchema,
+});
 
 export type ProjectParams = z.infer<typeof projectParamsSchema>;
 
