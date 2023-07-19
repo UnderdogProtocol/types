28601f354680a1d66bf1db83c2ac2a4a29a028e2
Kevin Arifin <kevarifin14@gmail.com>
2023-07-19 12:51:26 -0700

    types for org stats endpoint

--- src/validations/org.ts
+++ src/validations/org.ts
@@ -16,15 +16,20 @@ export type GetOrgsRequest = z.infer<typeof getOrgsRequestSchema>;
 export const getOrgsResponseSchema = orgPaginatedResponseSchema;
 export type GetOrgsResponse = z.infer<typeof getOrgsResponseSchema>;
 
-export const getOrgRequestSchema = z.object({
-  params: z.object({
-    orgId: idSchema,
-  }),
-});
+export const getOrgRequestSchema = z.object({ params: z.object({ orgId: idSchema }) });
 export type GetOrgRequest = z.infer<typeof getOrgRequestSchema>;
 
 export const getOrgResponseSchema = orgSchema;
 export type GetOrgResponse = z.infer<typeof getOrgResponseSchema>;
+
+export const getOrgStatsRequestSchema = z.object({ params: z.object({ orgId: idSchema }) });
+export type GetOrgStatsRequest = z.infer<typeof getOrgStatsRequestSchema>;
+
+export const getOrgStatsResponseSchema = z.object({
+  nfts: z.number(),
+  projects: z.number(),
+});
+export type GetOrgStatsResponse = z.infer<typeof getOrgStatsResponseSchema>;
 
 export const updateOrgRequestSchema = z.object({
   params: z.object({
