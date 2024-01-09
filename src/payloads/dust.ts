106edcafbd104ea7da974e4822b5fdda9ec10e45
Kevin Arifin <kevarifin14@gmail.com>
2024-01-08 16:27:52 -0800

    add seller fee basis points to dust payload

--- src/payloads/dust.ts
+++ src/payloads/dust.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 
-import { metadataSchema } from "../models";
+import { metadataSchema, sellerFeeBasisPointsSchema } from "../models";
 
 export const createDustPayloadSchema = metadataSchema.merge(
   z.object({
     csvUrl: z.string(),
     treeAddress: z.string().optional(),
+    sellerFeeBasisPoints: sellerFeeBasisPointsSchema.optional(),
   })
 );
 
