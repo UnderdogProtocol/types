3a22f6897d1094e7904800c960e24ab5edbf3c37
Kevin Arifin <kevarifin14@gmail.com>
2024-01-22 12:50:22 -0800

    add token to shop payload

--- src/payloads/shop.ts
+++ src/payloads/shop.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
-import { metadataSchema, sellerFeeBasisPointsSchema } from "../models";
+import { base58PublicKeySchema, metadataSchema, sellerFeeBasisPointsSchema } from "../models";
 
 export const createShopPayloadSchema = metadataSchema.merge(
   z.object({
     price: z.number().min(0).nullable().default(0),
+    token: base58PublicKeySchema.optional(),
+    whitelist: base58PublicKeySchema.optional(),
     supply: z.number().min(0),
     sellerFeeBasisPoints: sellerFeeBasisPointsSchema.optional(),
     namespace: z.string().optional(),
-    treeAddress: z.string().optional(),
-    whitelist: z.string().optional(),
     expiredAt: z
       .string()
       .optional()
