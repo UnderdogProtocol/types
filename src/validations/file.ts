9a645caacfec3be7e2528b906eac5d337d2321e2
Kevin Arifin <kevarifin14@gmail.com>
2024-08-29 10:04:38 -0700

    get validation

--- src/validations/file.ts
+++ src/validations/file.ts
@@ -18,3 +18,11 @@ export type CreateFileRequest = z.infer<typeof createFileRequestSchema>;
 export const createFileResponseSchema = registry.register("CreateFileResponse", fileSchema);
 
 export type CreateFileResponse = z.infer<typeof createFileResponseSchema>;
+
+export const getFilesRequestSchema = registry.register("GetFilesRequest", z.object({}));
+
+export type GetFilesRequest = z.infer<typeof getFilesRequestSchema>;
+
+export const getFilesResponseSchema = registry.register("GetFilesResponse", z.array(fileSchema));
+
+export type GetFilesResponse = z.infer<typeof getFilesResponseSchema>;
